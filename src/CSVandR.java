dc7329e48229bf53d7199fba06c7d8dbf57ab635
bleblanc2 <bleblanc2@ithaca.edu>
2017-06-12 12:30:28 -0400

    CSVTests

--- src/CSVandR.java
+++ src/CSVandR.java
@@ -1,6 +1,55 @@
+import com.github.rcaller.rstuff.*;
+import com.github.rcaller.util.Globals;
+
 /**
  * Created by Ben on 6/9/2017.
  */
 public class CSVandR {
+
+
+    public static double rowTest(){
+        RCaller rCaller;
+        if(Globals.isWindows() == false) {
+            RCallerOptions options = RCallerOptions.create("/usr/local/Cellar/r/3.4.0_1/bin/Rscript", Globals.R_current, FailurePolicy.RETRY_5, Long.MAX_VALUE, 100, RProcessStartUpOptions.create());
+            rCaller = RCaller.create(options);
+        }else {
+            rCaller  = RCaller.create();
+        }
+        RCode code = RCode.create();
+        code.addInt("points", 0);
+        code.addInt("max", 181);
+        code.addDouble("grade", 0);
+        /** Reading csvfile through hardcoded filepath
+         *  Read from directory path and filename as variable works too
+         */
+
+        code.addRCode("example.df <- read.csv(file='/Users/bleblanc2/IdeaProjects/javar/src/DataCSVExample.csv', header=TRUE, sep=',')");
+        code.addRCode("for(i in 3:11){ " +
+            "points <- points + example.df[[i]][2]" +
+                    "}");
+        code.addRCode("grade <- points/max");
+        rCaller.setRCode(code);
+        rCaller.runAndReturnResult("grade");
+        double[] results = rCaller.getParser().getAsDoubleArray("grade");
+        return results[0];
+    }
+
+    public static double columnTest(){
+        RCaller rCaller;
+        if(Globals.isWindows() == false) {
+            RCallerOptions options = RCallerOptions.create("/usr/local/Cellar/r/3.4.0_1/bin/Rscript", Globals.R_current, FailurePolicy.RETRY_5, Long.MAX_VALUE, 100, RProcessStartUpOptions.create());
+            rCaller = RCaller.create(options);
+        }else {
+            rCaller  = RCaller.create();
+        }
+        RCode code = RCode.create();
+        code.addDouble("classAvg", 0.0);
+        code.addRCode("example.df <- read.csv(file='/Users/bleblanc2/IdeaProjects/javar/src/DataCSVExample.csv', header=TRUE, sep=',')");
+        code.addRCode("classAvg <- mean(example.df$FinalExam)");
+        rCaller.setRCode(code);
+        rCaller.runAndReturnResult("classAvg");
+        double[] results = rCaller.getParser().getAsDoubleArray("classAvg");
+        return results[0];
+    }
 
 }
--- test/CSVandRTest.java
+++ test/CSVandRTest.java
@@ -1,6 +1,9 @@
 import org.junit.Test;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.text.DecimalFormat;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Ben on 6/9/2017.
@@ -8,8 +11,16 @@ import java.io.FileInputStream;
 public class CSVandRTest {
 
     @Test
-    public void rowTest(){
+    public void testRow(){
+        double expectedGrade = (145.0/181.0);
+        //Note on delta: R rounds to the 15 decimals which may cause problems in equals.
+        //In this case, Java has 16 decimals and R rounds the final digit to 15 decimals
+        assertEquals(expectedGrade, CSVandR.rowTest(), 0.01);
+    }
 
-
+    @Test
+    public void testColumn(){
+        double expectedAvg = (1861.5/25.0);
+        assertEquals(expectedAvg, CSVandR.columnTest(), 0.0);
     }
 }
